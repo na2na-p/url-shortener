0f524f3fa0e098a3b2af4f0e4cad729fccd5e978
na2na-p <na2na@na2na.dev>
2023-11-15 16:27:59 +0900

    コメント除去

--- src/main.rs
+++ src/main.rs
@@ -17,28 +17,18 @@ impl reject::Reject for InternalServerError {}
 
 #[tokio::main]
 async fn main() {
-    // 環境変数をロードする
     dotenv::dotenv().ok();
 
-    // Juniperのスキーマを作成
     let schema = create_schema();
+    let context_filter = warp::any().map(move || Context { /* 初期化 */ }).boxed();
 
-
-    let context_filter = warp::any().map(move || Context { /* コンテキストの初期化 */ }).boxed();
-
-
-    // GraphQLのエンドポイント
     let graphql_route = warp::path("api")
-        .and(warp::post())
         .and(make_graphql_filter(schema, context_filter));
 
-    // 短縮URLのリダイレクト
     let redirect_route = warp::path!("r" / String)
         .and_then(handlers::redirect::redirect_short_url);
 
-    // すべてのルートを組み合わせる
     let routes = graphql_route.or(redirect_route);
 
-    // サーバーを起動
     warp::serve(routes).run(([0, 0, 0, 0], 3030)).await;
 }
