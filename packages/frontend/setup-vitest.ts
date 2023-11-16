eb74a49ef27460edf837536c60fd67e9623e6fb0
na2na-p <na2na@na2na.dev>
2023-11-16 14:18:39 +0900

    cleanup

---
+++ packages/frontend/setup-vitest.ts
@@ -0,0 +1,4 @@
+import matchers from '@testing-library/jest-dom/matchers';
+import { expect } from 'vitest';
+import '@testing-library/jest-dom';
+expect.extend(matchers);
--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    "import.meta.vitest": "undefined",
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "setup-vitest.ts",
+    /**
+     * for in-source testing.
+     * {@link https://vitest.dev/guide/in-source.html}
+     */
+    includeSource: ["{src,integrationTest,docs}/**/*.{ts,tsx}"],
+  },
+  resolve: {
+    alias: {
+      "@/": `${__dirname}/src/`,
+      "@root/": `${__dirname}/`,
+    },
+  },
+});
